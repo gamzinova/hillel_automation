86499e75f9b2dc2526a2d6516d2270f7259eb863
gamzinova <gamzinova@gmail.com>
2022-11-28 23:49:53 +0200

    [HW20] Api testing - 6 tests

--- Anna_Gamzinova/api_collections/people_collection.py
+++ Anna_Gamzinova/api_collections/people_collection.py
@@ -2,7 +2,7 @@ import json
 from os import path
 
 from Anna_Gamzinova.api_collections.base_api import BaseAPI
-from Anna_Gamzinova.api_collections.config_file import BASE_URL
+
 
 
 class PeopleAPI(BaseAPI):
@@ -10,21 +10,21 @@ class PeopleAPI(BaseAPI):
         super().__init__()
         self.__person_url = 'public/v2/users'
 
-    def get_person(self, person_id):
+    def get_person(self, person_id: int):
         return self.get(f'{self.__person_url}/{person_id}')
 
     def get_list_of_people(self):
         return self.get(f'{self.__person_url}')
 
-    def create_person(self, person_data):
+    def create_person(self, person_data: dict):
         json_data = json.dumps(person_data)
         response = self.post(url=f'{self.__person_url}', body=person_data)
         return response
 
-    def delete_person(self, person_id):
-        response = self.delete(url=path.join(self.__person_url, person_id))
+    def delete_person(self, person_id: int):
+        response = self.delete(url=path.join(self.__person_url, str(person_id)))
         return response
 
-    def update_person(self, person_id, person_data):
-        response = self.patch(url=path.join(self.__person_url, person_id), body=person_data)
+    def update_person(self, person_id: int, person_data: dict):
+        response = self.patch(url=path.join(self.__person_url, str(person_id)), body=person_data)
         return response
--- Anna_Gamzinova/api_collections/test_api.py
+++ Anna_Gamzinova/api_collections/test_api.py
@@ -1,29 +1,26 @@
 from http import HTTPStatus
 
-import requests
-
-
 from Anna_Gamzinova.api_collections.people_collection import PeopleAPI
 
 
 def test_get_person_200():
-    response = PeopleAPI().get_person(1)
+    response = PeopleAPI().get_person(300)
+    print(response.json())
     assert response.status_code == HTTPStatus.OK, f'Status code is not as expected\n Actual: {response.status_code}' \
                                                   f'\nExpected: {HTTPStatus.OK}'
 
 
 def test_get_people_200():
     response = PeopleAPI().get_list_of_people()
-    print(response.content)
     assert response.status_code == HTTPStatus.OK, f'Status code is not as expected\n Actual: {response.status_code}' \
                                                   f'\nExpected: {HTTPStatus.OK}'
 
 
 def test_create_person():
     new_user = {
-        "name": "Nelson",
-        "gender": "female",
-        "email": "nchanukaissoon@gmail.com",
+        "name": "Santa Clause",
+        "gender": "male",
+        "email": "hohoho1@gmail.com",
         "status": "active"}
     response = PeopleAPI().create_person(new_user)
     assert response.status_code == HTTPStatus.CREATED, \
@@ -31,11 +28,27 @@ def test_create_person():
         f'\nExpected: {HTTPStatus.CREATED}'
 
 
+def test_create_person_name_check():
+    """
+    A test checks that the user ws created with the requested name
+    """
+    new_user = {
+        "name": "Mickie Mouse",
+        "gender": "male",
+        "email": "hellomickie1@gmail.com",
+        "status": "active"}
+    response = PeopleAPI().create_person(new_user)
+    person_values = response.json().values()
+    user_value = new_user.values()
+    assert list(user_value)[1] in person_values, \
+        f'A user was created with a wrong name'
+
+
 def test_create_existing_person():
     existing_user = {
-        "name": "Nelson",
-        "gender": "female",
-        "email": "nchanukaissoon@gmail.com",
+        "name": "Santa Clause",
+        "gender": "male",
+        "email": "hohoho1@gmail.com",
         "status": "active"}
     response = PeopleAPI().create_person(existing_user)
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY, \
@@ -44,37 +57,47 @@ def test_create_existing_person():
 
 
 def test_delete_existing_person():
-    response = PeopleAPI().delete_person(person_id='6934')
+    response = PeopleAPI().get_list_of_people()
+    response = PeopleAPI().delete_person(person_id=response.json()[0]['id'])
     assert response.status_code == HTTPStatus.NO_CONTENT, \
         f'Status code is not as expected\n Actual: {response.status_code}' \
         f'\nExpected: {HTTPStatus.NO_CONTENT}'
 
 
 def test_delete_non_existing_person():
-    response = PeopleAPI().delete_person(person_id='6934')
-    assert response.status_code == HTTPStatus.NO_CONTENT, \
+    response = PeopleAPI().delete_person(person_id=2747000000)
+    assert response.status_code == HTTPStatus.NOT_FOUND, \
         f'Status code is not as expected\n Actual: {response.status_code}' \
-        f'\nExpected: {HTTPStatus.NO_CONTENT}'
+        f'\nExpected: {HTTPStatus.NOT_FOUND}'
 
 
 def test_update_existing_person():
     update_user = {
-        "name": "Nelson",
-        "gender": "female",
-        "email": "nchanukaissoon@gmail.com",
+        "name": "Flanders",
+        "gender": "male",
+        "email": "yesiamflanders@gmail.com",
         "status": "active"}
-    response = PeopleAPI().update_person(person_id='6925', person_data=update_user)
+    response = PeopleAPI().get_list_of_people()
+    response = PeopleAPI().update_person(person_id=response.json()[0]['id'], person_data=update_user)
     assert response.status_code == HTTPStatus.OK, \
         f'Status code is not as expected\n Actual: {response.status_code}' \
         f'\nExpected: {HTTPStatus.OK}'
 
 
-# def test_get_people_2001():
-#     response = get('https://gorest.co.in/public/v2/users')
-#     assert response.status_code == HTTPStatus.OK, f'Status code is not as expected\n Actual: {response.status_code}' \
-#                                                   f'\nExpected: {HTTPStatus.OK}'
+def test_update_non_existing_person():
+    update_user = {
+        "name": "Nelson",
+        "gender": "male",
+        "email": "haha@gmail.com",
+        "status": "active"}
+    response = PeopleAPI().update_person(person_id=2747000000, person_data=update_user)
+    assert response.status_code == HTTPStatus.NOT_FOUND, \
+        f'Status code is not as expected\n Actual: {response.status_code}' \
+        f'\nExpected: {HTTPStatus.NOT_FOUND}'
 
-def test_get_people_2002():
-    response = requests.post('https://gorest.co.in/public/v2/users', params=dictionary)
-    assert response.status_code == HTTPStatus.CREATED, f'Status code is not as expected\n Actual: {response.status_code}' \
-                                                       f'\nExpected: {HTTPStatus.CREATED}'
+
+def test_keys():
+    response = PeopleAPI().get_list_of_people()
+    person_keys = response.json()[0].keys()
+    dict_keys = {'id': '', 'name': '', 'email': '', 'gender': '', 'status': ''}
+    assert person_keys == dict.keys(dict_keys), f'Dictionary keys are incorrect'
