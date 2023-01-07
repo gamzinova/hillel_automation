f5cb513e0119af08047e058306fd45140cfdfe12
gamzinova <gamzinova@gmail.com>
2023-01-07 12:20:02 +0200

    [framework]headless mode is set

--- Anna_Gamzinova/framework_from_scratch/utilities/driver_factory.py
+++ Anna_Gamzinova/framework_from_scratch/utilities/driver_factory.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.chrome.options import Options
 
 
 class DriverFactory:
@@ -11,9 +12,12 @@ class DriverFactory:
     EDGE = 3
 
     @staticmethod
-    def create_driver(driver_id: int):
-        if int(driver_id) == DriverFactory.CHROME:
-            driver = Chrome(service=Service(ChromeDriverManager().install()))
+    def create_driver(driver_id: int, is_headless=True, chrome_options=None):
+        if int(driver_id) == 1:
+            chrome_options == Options()
+            if is_headless:
+                chrome_options.add_argument("--headless")
+            driver = Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         elif int(driver_id) == DriverFactory.FIREFOX:
             driver = Firefox(service=Service(GeckoDriverManager().install()))
         elif int(driver_id) == DriverFactory.EDGE:
